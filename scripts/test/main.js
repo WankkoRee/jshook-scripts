f8c47dc7d32df2bdc49625a0b61e2aba94f3311d
WankkoRee <wkr@wkr.moe>
2023-09-24 16:44:24 +0000

    release: (d2d4465) feat: 适配 tsx

---
+++ scripts/test/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(runtime.appInfo);
+console.log(runtime.classLoader);
+console.log(runtime.coreType === 1 /* CoreTypeN.rhino */);
